59f3466d6003f20a0788411b50524a885de7360a
Boris Tatarchuk <bob.king.90@mail.ru>
2023-11-08 00:59:17 +0300

    Create function.js

---
+++ function.js
@@ -0,0 +1,8 @@
+const node_for_click = document.getElementById("for_click");
+
+function find_edit(){
+    const p1  = document.getElementById("LM").textContent = "Tatarchuk";
+    const p2  = document.getElementById("FM").textContent = "Boris";
+}
+
+node_for_click.addEventListener("click",find_edit);
